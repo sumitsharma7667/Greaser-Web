601b490c619245c4b5ce4026a88acfa246da0a7f
macbook <sumitsharma7667@gmail.com>
2021-05-07 19:29:55 +0530

    df

--- src/Services/ServiceType.js
+++ src/Services/ServiceType.js
@@ -1,10 +1,12 @@
 import React,{useState,useEffect} from 'react';
-
+import DataTable from '@bit/adeoy.utils.data-table';
+import $ from 'jquery';
 const ServiceType =()=>{
     const[name,SetName]=useState()
     const[image,SetImage]=useState()
     const[status,SetStatus]=useState(0)
-    const[AllServices,SetAllServiceType]=useState([])
+    const[_id,SetId]=useState()
+    const[AllServicesType,SetAllServiceType]=useState([])
 
     useEffect(()=>{
     GetServiceType()
@@ -28,7 +30,34 @@ const ServiceType =()=>{
             .then(err => {})
         // }
     }
-
+    const EditObj = (obj) => {
+        $("html").scrollTop(0);
+        SetId(obj._id)
+         SetName(obj.name)
+         SetStatus(obj.status)
+         SetImage(obj.image)
+     }
+    const UpdateServiceType =()=>{
+         // if (this.checkValidation()) {
+         const data = new FormData()
+         data.append('_id', _id)
+         data.append('name', name)      
+         data.append('status', status)
+         data.append('image',image)
+         const url="http://144.91.110.221:3032/UpdateServiceType"
+                 fetch(url,
+                     {
+                     method:'put',
+                     body:data
+                 })
+                 .then(res => res.json())              
+                 .then((res)=>{  
+                alert('Service updated successfully !');
+                GetServiceType()
+                 })            
+                 .then(err=>console.log(err))
+             //   }
+       }
     const GetServiceType = () => {
         fetch("http://144.91.110.221:3032/GetServiceType")
             .then(res => res.json())
@@ -38,6 +67,22 @@ const ServiceType =()=>{
             })
             .then(err => console.log(err))
     }
+
+    // data table
+
+    const data =[];
+    {AllServicesType.map((item,index)=>{
+    data.push( {"sr_no":index+1,"name":item.name,"image":<img src={"http://144.91.110.221:3032/" + item.image} style={{ height: '100px' }} />,"action":<span><button className="btn btn-secondary" onClick={()=>{EditObj(item)}}> Edit</button> </span>})
+    })}
+      const columns = [
+        { title: "SR NO", data: "sr_no" },
+        { title: "Name", data: "name" },
+        { title: "Image", data: "image" },
+        { title: "Action", data: "action" },
+      ];
+      const click = (row) => {
+        console.log(row);
+      };
     return(
         <>
 <div class="container-fluid">
@@ -50,7 +95,7 @@ const ServiceType =()=>{
             {/* <form> */}
            <div class="form-group">
             <label for="input-1">Name</label>
-            <input type="text" class="form-control" onChange={(e)=>{SetName(e.target.value)}} placeholder="Enter Brand Name" />
+            <input type="text" class="form-control" value={name} onChange={(e)=>{SetName(e.target.value)}} placeholder="Enter Brand Name" />
            </div>
            <div class="form-row">
            <div class="col-lg-6 form-group">
@@ -59,7 +104,7 @@ const ServiceType =()=>{
            </div>
            <div class="col-lg-6 form-group"  >
            <label for="input-2">Status</label>
-                <select className="form-control" onChange={(e)=>{SetStatus(e.target.value)}}>
+                <select className="form-control" value={status} onChange={(e)=>{SetStatus(e.target.value)}}>
                 
                     <option value="0">Active</option>
                     <option value="1">Deacive</option>
@@ -67,13 +112,25 @@ const ServiceType =()=>{
             </div>
             </div>
            <div class="form-group">
-            <button type="submit" class="btn btn-light px-5" onClick={()=>{StoreServiceType()}}><i class="icon-lock"></i> Submit</button>
+            <button type="submit" class="btn btn-light px-5" onClick={()=>{StoreServiceType()}}><i class="icon-lock"></i> Submit</button><button type="submit" class="btn btn-default px-5" onClick={()=>{UpdateServiceType()}}><i class="icon-lock"></i> Update</button>
           </div>
           {/* </form> */}
          </div>
          </div>
       </div>
     </div> 
+    <div class="row mt-3">
+             <div className="col-12">
+                <DataTable
+                data={data}
+                columns={columns}
+                striped={true}
+                hover={true}
+                responsive={true}
+                onClickRow={click}
+                />
+            </div>
+    </div>
     {/* // <!--End Row--> */}
 
 	{/* // <!--start overlay--> */}
